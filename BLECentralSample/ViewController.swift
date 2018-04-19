9e63c364a8139c281f9df7bc859d6e6d5ba6a323
高井翔太 <takaishota@gmail.com>
2018-04-19 20:46:18 +0900

    Initial Commit

---
+++ BLECentralSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLECentralSample
+//
+//  Created by 高井　翔太 on 2018/04/19.
+//  Copyright © 2018年 Shota Takai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
