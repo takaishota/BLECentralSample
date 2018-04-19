3da9a53e1245be38006ce0919ea550d13c06cc38
高井翔太 <takaishota@gmail.com>
2018-04-19 21:43:24 +0900

    add scan peripheral

--- BLECentralSample/ViewController.swift
+++ BLECentralSample/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
-
+    var centralManager: CBCentralManager?
+    var cmPowerIsOn = false
+    var scanIsOn = false {
+        didSet {
+            if scanIsOn && cmPowerIsOn {
+                print("start scan")
+                centralManager?.scanForPeripherals(withServices: nil, options: nil)
+            } else {
+                print("stop scan")
+                centralManager?.stopScan()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func tapButton(_ sender: UIButton) {
+        scanIsOn = !scanIsOn
+    }
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("state: \(central.state)")
+        
+        switch central.state {
+        case .poweredOn:
+            cmPowerIsOn = true
+            print("central power is on")
+        default:
+            break
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("発見したBLEデバイス: \(peripheral)")
+    }
+}
